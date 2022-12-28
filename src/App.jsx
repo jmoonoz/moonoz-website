52f778a6f9929d397226fa942eb462830de9b5da
Joel Munoz <j.munoz1@yahoo.com>
2022-12-27 20:03:28 -0800

    pages created

--- src/App.jsx
+++ src/App.jsx
@@ -9,19 +9,23 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 import About from './pages/About'
 import Home from './pages/Home'
+import Projects from './pages/Projects';
+import Contact from './pages/Contact'
 import NavBar from './components/NavBar'
 
 function App() {
 
   return (
     <Router>
-        <NavBar />
-        <Routes>
-        <Route path="/About" element={<About/>}></Route>
-          <Route path="/" element={<Home />}></Route>
-        </Routes>
-        {/* <Footer /> */}
-      </Router>
+      <NavBar />
+      <Routes>
+        <Route path="/Projects" element={<Projects />}></Route>
+        <Route path="/Contact" element={<Contact />}></Route>
+        <Route path="/About" element={<About />}></Route>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+      {/* <Footer /> */}
+    </Router>
   )
 }
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,22 +1,25 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 
 export default function NavBar() {
+    const [activeLink, setActiveLink] = useState('/');
     return (
-        <Navbar fixed="top"  expand="lg">
+        <Navbar fixed="top" expand="lg">
             <Container>
-                <Navbar.Brand href="/">Moonoz
-                </Navbar.Brand>
+                <Navbar.Brand href="/">Moonoz</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" >
                     <span className="navbar-toggle-icon"></span>
                 </Navbar.Toggle>
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="/">Home</Nav.Link>
-                        <Nav.Link href="/About">About</Nav.Link>
+                    <Nav className="ms-auto">
+                        <div className="vr" />
+                        <Nav.Link href="/" className={activeLink === '/' ? "active navbar-link" : "navbar-link"}>Home</Nav.Link>
+                        <Nav.Link href="/About" className={activeLink === '/About' ? "active navbar-link" : "navbar-link"}>About</Nav.Link>
+                        <Nav.Link href="/Projects" className={activeLink === '/Projects' ? "active navbar-link" : "navbar-link"}>Projects</Nav.Link>
+                        <Nav.Link href="/Contact" className={activeLink === '/Contact' ? "active navbar-link" : "navbar-link"}>Contact</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
