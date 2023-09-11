3a683b0960e274584a78b1c9860b8c522a3d497c
Joel Munoz <j.munoz1@yahoo.com>
2023-09-10 19:13:30 -0700

    edited style name q Q

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./Style/App.css";
+// import "./Style/App.css";
+import "./Style/App.css"
 import "./Style/Responsive.css";
 import SocialMenu from "./Components/SocialMenu";
 import NavBar from "./Components/NavBar";
