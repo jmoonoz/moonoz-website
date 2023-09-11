2865449af5211f60087c91dba9db2b01c5140030
Joel Munoz <j.munoz1@yahoo.com>
2023-09-10 22:03:15 -0700

    added new file in public folder

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,12 +4,12 @@ import Hero from "../Components/Hero";
 import Contact from "../Components/ContactSection";
 import ProjectSection from "../Components/ProjectSection";
 
-
 export default function Home() {
+  const newURL = window.location.pathname;
+  console.log(newURL);
   return (
-      <div>
-        <Hero />
-      </div>
-
+    <div>
+      <Hero />
+    </div>
   );
 }
